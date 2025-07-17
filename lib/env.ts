d1b0c916a1cafa170efae7b9d1ef8bbbe1238f9a
inulpro <inulpro88@gmail.com>
2025-07-18 05:34:01 +0700

    course creation

--- lib/env.ts
+++ lib/env.ts
@@ -3,16 +3,16 @@ import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.url(),
+    DATABASE_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z.url(),
+    BETTER_AUTH_URL: z.string().url(),
     AUTH_GITHUB_CLIENT_ID: z.string().min(1),
     AUTH_GITHUB_SECRET: z.string().min(1),
     ARCJET_KEY: z.string().min(1),
     // Email server configuration
     EMAIL_SERVER_HOST: z.string().min(1),
     EMAIL_SERVER_PORT: z.string().default("465"),
-    EMAIL_SERVER_USER: z.email(),
+    EMAIL_SERVER_USER: z.string().email(),
     EMAIL_SERVER_PASSWORD: z.string().min(1),
   },
 
