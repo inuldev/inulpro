a3bcb0f281f6f78f6d0a78c22bd3ace3b5bcd163
inulpro <inulpro88@gmail.com>
2025-07-17 13:16:36 +0700

    login page ui

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,47 @@
+import { IconBrandGithub } from "@tabler/icons-react";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>hello from the login page</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardDescription>
+          Login with your Github or Email Account
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-4">
+        <Button className="w-full" variant="outline">
+          <IconBrandGithub className="size-4" />
+          Sign in with Github
+        </Button>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" placeholder="me@example.com" />
+          </div>
+
+          <Button>Continue with email</Button>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-red-500">Hello World</h1>
+
+      <ThemeToggle />
     </div>
   );
 }
